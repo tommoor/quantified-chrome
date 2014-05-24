20ae43fbe8af0573a733ff6ad6aaf92bbf5ea22d
Tom Moor <tom.moor@gmail.com>
2014-05-24 15:30:13 -0700

    wip

--- js/quantified.js
+++ js/quantified.js
@@ -1,9 +1,9 @@
 var db = new PouchDB('quantified-chrome', {adapter : 'websql'});
-var selected;
-var session = _.uniqueId();
+var selectedId;
+var session = 'tab-' + (new Date()).getTime();
 
 chrome.tabs.getSelected(function(tab) {
-    selected = tab;
+    selectedId = tab.id;
 });
 
 chrome.tabs.onCreated.addListener(function(tab){
@@ -11,54 +11,66 @@ chrome.tabs.onCreated.addListener(function(tab){
     recordTab(tab);
 });
 
-chrome.tabs.onUpdated.addListener(function(tab){
-    db.get(session+tab.id, function(err, existing){
-        if (!existing || tab.url !== existing.url) {
-            console.debug('Tab updated');
-            recordTab(tab);
+chrome.tabs.onUpdated.addListener(function(tabId, changes){
+    var _id = session+tabId;
+    
+    db.get(_id, function(err, existing){
+        console.log(existing);
+        console.log(changes);
+        
+        if (!existing || (existing.url && changes.url && (existing.url != changes.url))) {
+            
+            chrome.tabs.get(tabId, function(tab){
+                console.debug('Tab changed ' + _id);
+                recordTab(tab);
+            });
         }
     });
 });
 
-chrome.tabs.onActivated.addListener(function(tab){
-    db.get(session+tab.id, function(err, existing){
+chrome.tabs.onActivated.addListener(function(activeInfo){
+    var _id = session+activeInfo.tabId;
+    db.get(_id, function(err, existing){
         if (existing) {
             var data = {
                 selectedAt: (new Date()).getTime()
             };
             
-            console.debug('Tab activated', data);
-            db.put(data, session+tab.id, existing._rev);
+            console.debug('Tab activated ' + _id, data);
+            db.put(data, _id, existing._rev);
         }
     });
     
-    db.get(session+selected.id, function(err, existing){
-        if (existing) {
-            var now = (new Date()).getTime();
-            var data = {
-                timeSelected: timeSelected + (now-existing.selectedAt)
-            };
+    if (selectedId) {
+        var _id = session+selectedId;
+        db.get(_id, function(err, existing){
+            if (existing) {
+                var now = (new Date()).getTime();
+                var data = {
+                    timeSelected: existing.timeSelected + (now-existing.selectedAt)
+                };
             
-            console.debug('Tab deactivated', data);
-            db.put(data, session+selected.id, existing._rev);
-        }
-    });
+                console.debug('Tab deactivated ' + _id, data);
+                db.put(data, _id, existing._rev);
+            }
+        });
+    }
     
-    selected = tab;
+    selectedId = activeInfo.tabId;
 });
 
-chrome.tabs.onRemoved.addListener(function(tab){
-    db.get(session+tab.id, function(err, existing){
+chrome.tabs.onRemoved.addListener(function(tabId){
+    var _id = session+tabId;
+    db.get(_id, function(err, existing){
         if (existing) {
             var now = (new Date()).getTime();
             var data = {
                 closedAt: now,
-                timeOpen: now-existing.openedAt,
-                timeSelected: tab.selected ? timeSelected + (now-existing.selectedAt) : existing.timeSelected
+                timeOpen: now-existing.openedAt
             };
             
-            console.debug('Closing tab', data);
-            db.put(data, session+tab.id, existing._rev);
+            console.debug('Closing tab ' + _id, data);
+            db.put(data, _id, existing._rev);
         }
     });
 });
@@ -73,15 +85,19 @@ var recordTab = function(tab) {
         return;
     }
     
+    var _id = session+tab.id;
     var data = {
         openedAt: (new Date()).getTime(),
         timeOpen: 0,
         timeSelected: 0,
         title: tab.title,
         favIconUrl: tab.favIconUrl,
-        hostname: hostname
+        hostname: hostname,
+        url: tab.url
     };
     
-    console.debug('Recording tab', data);
-    db.put(data, session+tab.id);
+    console.debug('Recording tab ' + _id, data);
+    db.put(data, _id, function(err, success){
+        if (err) console.warn(err);
+    });
 };
